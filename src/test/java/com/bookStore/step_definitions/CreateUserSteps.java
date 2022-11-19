5329686fc28d6fd808e2f724dc88c37a622ea595
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-11-19 18:53:17 +0000

    CreateUserSteps.java

--- src/test/java/com/bookStore/step_definitions/CreateUserSteps.java
+++ src/test/java/com/bookStore/step_definitions/CreateUserSteps.java
@@ -1,20 +1,49 @@
 package com.bookStore.step_definitions;
 
-import io.restassured.RestAssured;
+import com.bookStore.utilities.ConfigurationReader;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
 
 public class CreateUserSteps {
+    Response response;
+    String requestUsername;
+    String requestPassword;
 
-    String url = "https://demoqa.com";
+    @When("User sends a POST request to create user end point")
+    public void user_sends_a_POST_request_to_create_user_end_point() {
+        // Body part of the request
+        requestUsername = ConfigurationReader.getProperty("userName");
+        requestPassword = ConfigurationReader.getProperty("password");
+        Map<String, String> requestBody = new LinkedHashMap<>();
+        requestBody.put("userName", requestUsername);
+        requestBody.put("password", requestPassword);
 
-    @Test
-    public void test() {
+        // End point of request
+        baseURI = ConfigurationReader.getProperty("baseUrl");
+        basePath = ConfigurationReader.getProperty("apiUser");
 
-        Response response = RestAssured.get(url);
+        // Creating request to API
+        response = given().accept(ContentType.JSON)
+                .and().contentType(ContentType.JSON)
+                .and().body(requestBody)
+                .when().post();
+    }
 
-        System.out.println(response.statusCode());
-
+    @When("User captures status code, userID username information")
+    public void user_captures_status_code_userID_username_information() {
         response.prettyPrint();
     }
+
+    @Then("User verifies status code, username and userID is NOT null")
+    public void user_verifies_status_code_username_and_userID_is_NOT_null() {
+
+    }
+
 }
