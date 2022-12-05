481589ad61b3f4e87b82a72f583e41f3dcf538dd
Nrfsnozdemir <avnurefsanozdemir@gmail.com>
2022-12-05 13:20:23 +0000

    Runner.java

--- src/test/java/com/bookStore/runners/Runner.java
+++ src/test/java/com/bookStore/runners/Runner.java
@@ -1,4 +1,4 @@
-package com.bookStore.runner;
+package com.bookStore.runners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -8,13 +8,16 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 //"pretty",
-                "json:target/cucumber-report/cucumber.json",
-                "html:target/cucumber-report.html"
+                "json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                //"me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
         glue = "com/bookStore/step_definitions",
         dryRun = false,
-        tags = " "
+        tags = ""
 )
+
 public class Runner {
 }
